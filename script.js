c55851b876a0ce973ed8bc0e7a978d74e004e611
Eyouel <eeyoueltsegaye@gmail.com>
2022-08-14 20:02:33 +0300

    partially edited converting weather code
    converted the weather code into a clear description

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let apiEndpoint = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true';
+let apiEndpoint = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true&timezone=auto&daily=weathercode';
 async function weather(){
     try{
     let responseObject = await fetch(apiEndpoint);
@@ -7,11 +7,16 @@ async function weather(){
     <div class="card" style="width: 18rem;">
         <img src="sunny.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-    <h5 class="card-title">today</h5>
-    <p class="card-text">Temprature:${data.current_weather.temperature} </p>
+    <h5 class="card-title">TODAY</h5>
+    <p class="card-text">weather ${weatherCodeConverter(data.current_weather.weathercode)}</p>
+    <p class="card-text">Temprature ${data.current_weather.temperature} </p>
+    <p class="card-text">Wind Direction ${data.current_weather.winddirection} </p>
+    <p class="card-text">Wind Speed ${data.current_weather.windspeed} </p>
     </div>
     </div>
-    `
+    `;
+    weatherCodeConverter(data.current_weather.weathercode);
+    
     console.log(data);
     console.log(data.current_weather.temperature);
     }
@@ -20,4 +25,48 @@ async function weather(){
     }
 }
 weather();
+function weatherCodeConverter(code){   
+    if(code==0){
+       return "Clear sky" ;
+    }
+    else if(code==1||code==2||code==3){
+        return "Mainly clear, partly cloudy, and overcast" ;
+    }
+    else if(code==45||code==48){
+        return "Fog and depositing rime fog" ;
+    }
+    else if(code==51||code==53||code==55){
+        return "Drizzle: Light, moderate, and dense intensity" ;
+    }
+    else if(code==56||code==57){
+        return "	Freezing Drizzle: Light and dense intensity" ;
+    }
+    else if(code==61||code==63||code==65){
+        return "Rain: Slight, moderate and heavy intensity" ;
+    }
+    else if(code==66||code==67){
+        return "Freezing Rain: Light and heavy intensity" ;
+    }
+    else if(code==71||code==73||code==75){
+        return "Snow fall: Slight, moderate, and heavy intensity" ;
+    }
+    else if(code==77){
+        return "Snow grains" ;
+    }
+    else if(code==80||code==81||code==82){
+        return "Rain showers: Slight, moderate, and violent" ;
+    }
+    else if(code==85||code==86){
+        return "Snow showers slight and heavy" ;
+    }
+    else if(code==95){
+        return "Thunderstorm: Slight or moderate" ;
+    }
+    else if(code==96||code==99){
+        return "Thunderstorm with slight and heavy hail" ;
+    }
+    else{
+        return '';
+    }
+}
 
